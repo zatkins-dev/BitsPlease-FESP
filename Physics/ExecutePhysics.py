29c6ceb35f5f4f398ef0b4a94912bef6b608ef0b
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-13 17:05:54 -0500

    created basic exec script, loads world with a floor

---
+++ Physics/ExecutePhysics.py
@@ -0,0 +1,48 @@
+import pygame as pg
+import pymunk as pm
+import pymunk.pygame_util as pygame_util
+import sys
+
+res_x, res_y = 1000, 1000
+EARTH_MASS = 5.97*10**24
+EARTH_RADIUS = 6371000
+EARTH_MOMENT = pm.moment_for_circle(EARTH_MASS, 0, EARTH_RADIUS)
+GROUND_Y = res_y/10
+G = 6.67408*10**-11
+
+
+def keyDown(e, key):
+    return e.type == pg.KEYDOWN and e.key == key
+
+
+def keyUp(e, key):
+    return e.type == pg.KEYUP and e.key == key
+
+
+def run():
+    pg.init()
+    screen = pg.display.set_mode((res_x, res_y))
+    clock = pg.time.Clock()
+
+    space = pm.Space()
+    # earthBody = pm.Body(EARTH_MASS, EARTH_MOMENT, pm.Body.STATIC)
+    groundLine = pm.Segment(
+        space.static_body, (0, GROUND_Y), (1000, GROUND_Y), 3
+    )
+    space.add(groundLine)
+    draw_options = pygame_util.DrawOptions(screen)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT or keyDown(event, pg.K_ESCAPE):
+                sys.exit(0)
+
+        space.step(1/50.0)
+        screen.fill((255, 255, 255))
+        space.debug_draw(draw_options)
+        pg.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+    run()
