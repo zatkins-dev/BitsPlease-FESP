18817562e5c5c5e5a08d429fb70561bbf9eec1ba
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-14 18:55:15 -0500

    edited testRocket to use modular components

--- Physics/TestRocket.py
+++ Physics/TestRocket.py
@@ -1,43 +1,32 @@
 import pygame as pg
-import pymunk as pm
-import pymunk.pygame_util as pygame_util
-from pymunk.vec2d import Vec2d
-import random
+from component import Component
+from thruster import Thruster
+from rocket import Rocket
 
 
-def genOrbiter(body):
+def genOrbiter(b):
     density = 34
     verts = [(12, 4), (-12, 4), (0, 42)]
-    orbiter = pm.Poly(body, verts, radius=1)
+    orbiter = Component(b, verts, radius=1)
     orbiter.density = density
     return orbiter
 
 
-def genTank(body):
+def genTank(b):
     verts = [(4.2, 0), (-4.2, 0), (4.2, 46.9), (-4.2, 46.9)]
     density = 73.58
-    tank = pm.Poly(body, verts, radius=1)
+    tank = Thruster(b, verts, (0, 1), 5255000, radius=1)
     tank.density = density
+    tank.key = pg.K_f
     return tank
 
 
 def genRocket(space):
-    body = pm.Body()
-    components = [genTank(body), genOrbiter(body)]
-    space.add(body)
+    rocket = Rocket()
+    components = [genTank(rocket), genOrbiter(rocket)]
+    for c in components:
+        rocket.addComponent(c)
+    space.add(rocket)
     for c in components:
         space.add(c)
-    return body
-
-
-def getRocketThrust(rocket, netThrust):
-    angle = rocket.rotation_vector
-    # random.seed()
-    angle_perturbation = random.normalvariate(0, 0.001)
-    thrust = Vec2d(0, netThrust)
-    print("original: {0}\n", thrust)
-    thrust.cpvrotate(angle)
-    print("based on ship angle: {0}\n", thrust)
-    thrust.rotate(angle_perturbation)
-    print("perturbed: {0}\n", thrust)
-    return thrust
+    return rocket
