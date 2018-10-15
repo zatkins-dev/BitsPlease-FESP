79dbc040a38727c216585dd771b61cabfdcc7e33
Gage Phillips <gagerdude@gmail.com>
2018-10-15 18:20:20 -0500

    Created a Physics helper class, and added a function to calc gravity

---
+++ Physics/Physics.py
@@ -0,0 +1,26 @@
+import math
+
+class Physics(object):
+
+    _GRAV_CONSTANT = 6.67384*(10**-11)
+
+    @staticmethod
+    #Calculates the force due to gravity between a body and a target
+    def gravity(body, target):
+        #First, find the distance between the body and the target
+        #Then, Use that distance to calculate gravity
+        dX = body.position[0] - target.position[0]
+        dY = body.position[1] - target.position[1]
+
+        rSquared = dX**2 + dY**2
+
+        #Now, find force of gravity in the direction of R
+        forceMagnitude = Physics._GRAV_CONSTANT * body.mass * target.mass / rSquared
+
+        #Find the angle between these two so that this can be translated back to Cartesian Coords
+        angle = math.atan2(dY, dX)
+
+        fX = forceMagnitude * math.cos(angle)
+        fY = forceMagnitude * math.sin(angle)
+
+        return (fX, fY)
