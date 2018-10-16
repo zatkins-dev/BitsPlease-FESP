e2e3a6fba542aba1452cdd86107465c922d4a5a3
marshingjay <jam605@gmail.com>
2018-10-16 16:24:45 -0500

    SAS module now functions correctly

--- Physics/SAS.py
+++ Physics/SAS.py
@@ -1,24 +1,38 @@
-import pymunk.vec2d import vec2d
 import pygame as pg
 from component import Component
 
 class SAS(Component):
-    def __init__(self, body, vertices, SASdirection, SASforce, transform=None, radius=0):
-        self._SASdirection = 0
+    def __init__(self, body, vertices, SASforce, SASpower, angle, transform=None, radius=0):
+        Component.__init__(self, body, vertices, transform, radius)
+        self._SASangle = 0
+
+        #SASforce: 0 is continue going straight, + turns the rocket left, - turns the rocket right
+        #the greater the force the more the rocket turns
         self._SASforce = 0
-        self.leftKey = pg.K_a
-        self.rightKey = pg.K_d
-    
-    def SAS_turning(self):
-        return self._SASdirection * self._SASforce
+
+        #SASpower: rate at which the rockets SASforce increments when given input from the user
+        self._SASpower = 0.05
+
+
+        self.leftKey = None
+        self.rightKey = None
 
     @property
-    def SASdirection(self):
-        return self._SASdirection
+    def SASpower(self):
+        return self._SASpower
     
-    @SASdirection.setter
-    def SASdirection(self, newDir):
-        self._SASdirection = newDir    
+    @SASpower.setter
+    def SASpower(self, newPower):
+        self._SASpower = newPower  
+
+
+    @property
+    def SASangle(self):
+        return self._SASangle
+    
+    @SASangle.setter
+    def SASangle(self, newAngle):
+        self._SASangle = newAngle   
 
     @property
     def SASforce(self):
--- Physics/rocket.py
+++ Physics/rocket.py
@@ -3,6 +3,7 @@ import pygame as pg
 import pymunk as pm
 from component import Component
 from thruster import Thruster
+from SAS import SAS
 
 
 class Rocket(Body):
@@ -12,23 +13,27 @@ class Rocket(Body):
             c.body = self
         self.components = components
         self.thrusters = filter(lambda c: type(c) == Thruster, self.components)
-        self.rotationLeftKey = pg.K_a
-        self.rotationRightKey = pg.K_d
+        self.SASmodules = filter(lambda c: type(c) == SAS, self.components)
         self.angular_velocity_limit = 400000
 
     def thrust(self, k):
-        for t in self.thrusters:
+        for t in self.components:
             if t.key is None:
                 continue
             if t.key == k:
                 self.apply_impulse_at_local_point(t.thrust(), (0, 0))
+                
 
-    def rotate(self, k):
-        if self.rotationLeftKey == k:
-            self.angular_velocity = self.angular_velocity + 0.05
-
-        if self.rotationRightKey == k:
-            self.angular_velocity = self.angular_velocity - 0.05
-
+    def turn_SAS(self, k):
+        for m in self.components:
+            if not isinstance(m, SAS):
+                continue
+            else :    
+                if m.leftKey == k:
+                    m.SASforce = m.SASforce + m.SASpower
+            
+                if m.rightKey == k:
+                    m.SASforce = m.SASforce - m.SASpower
+                self.angular_velocity = m.SASforce
     def addComponent(self, c):
         self.components.append(c)
