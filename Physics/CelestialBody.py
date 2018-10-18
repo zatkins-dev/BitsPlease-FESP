c441b8d99425572394272db10d7049561d682cc2
marshingjay <jam605@gmail.com>
2018-10-18 17:32:51 -0500

    made planets not kinematic again

--- Physics/CelestialBody.py
+++ Physics/CelestialBody.py
@@ -3,7 +3,7 @@ import pygame as pg
 
 
 class CelestialBody():
-    def __init__(self, name_, space_, mass_, radius_, position_x, position_y, elasticity_, atmosphere_, bodytype_):
+    def __init__(self, name_, space_, mass_, radius_, position_x, position_y, elasticity_, atmosphere_, bodytype):
         self.mass = mass_
         self.name = name_
         self.radius = radius_
@@ -12,7 +12,10 @@ class CelestialBody():
         self.elasticity = elasticity_
         self.atmosphere = atmosphere_
         self.bodytype = bodytype_
-        self.body = pm.Body(self.bodytype)
+        if self.bodytype == 0 :
+            self.body = pm.Body(body_type=pm.Body.STATIC)
+        else:
+            self.body = pm.Body(body_type=pm.Body.KINEMATIC)
         self.shape = pm.Circle(self.body, self.radius)
         self.shape.mass = self.mass
         self.shape.elasticity = self.elasticity
