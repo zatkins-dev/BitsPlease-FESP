36bf749808343ed3a4ce5202a1656b9c8bcf439e
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-10-20 09:25:51 -0500

    cleaned up rocket.py

--- Rockets/rocket.py
+++ Rockets/rocket.py
@@ -1,8 +1,5 @@
 from pymunk import Body as Body
 import pygame as pg
-import pymunk as pm
-import math as math
-from Rockets.component import Component
 from Rockets.thruster import Thruster
 from Rockets.SAS import SAS
 
@@ -16,8 +13,7 @@ class Rocket(Body):
         self.thrusters = filter(lambda c: type(c) == Thruster, self.components)
         self.SASmodules = filter(lambda c: type(c) == SAS, self.components)
         self.angular_velocity_limit = 400000
-    
-    
+
     def thrust(self, k):
         for t in self.components:
             if t.key is None:
@@ -26,26 +22,24 @@ class Rocket(Body):
                 if t.fuel > 0:
                     self.apply_impulse_at_local_point(t.thrust(), (0, 0))
                     t.fuel -= 1
-                
-                
 
     def turn_SAS(self, k, coeffPower):
         for m in self.components:
             if not isinstance(m, SAS):
                 continue
-            else :
-                if m.fuel > 0 :
+            else:
+                if m.fuel > 0:
                     if m.leftKey == k:
                         self.angular_velocity += m.SASpower * coeffPower
                         m.fuel -= 1 * coeffPower
-                
+
                     if m.rightKey == k:
                         self.angular_velocity -= m.SASpower * coeffPower
                         m.fuel -= 1 * coeffPower
                 else:
-                    #you silly goose 
+                    # you silly goose
                     print('SAS module is out of fuel')
-    
+
     def auto_SAS(self, targetAngle):
         if targetAngle > self.angle:
             self.turn_SAS(pg.K_a, 0.25)
@@ -53,6 +47,7 @@ class Rocket(Body):
             self.turn_SAS(pg.K_d, 0.25)
         else:
             pass
-            #do nothing, on course
+            # do nothing, on course
+
     def addComponent(self, c):
         self.components.append(c)
