ee3b81a3224223926501d70d64402d4d9bc9c0df
Gage Phillips <gagerdude@gmail.com>
2018-11-10 23:35:15 -0600

    Work on making Thruster an ABC, and creating a subclass
    This is only "kinda" what I want it to be. It turns out that the property and classmethod decorators don't play well together... I have the properties being stored on a class-level, with instance-level properties that call on the class-level ones. This makes the ABC implementation kinda clunky, so it may be removed.

--- rockets/thruster.py
+++ rockets/thruster.py
@@ -1,9 +1,12 @@
+from abc import ABC, abstractmethod
 from pymunk.vec2d import Vec2d
 from rockets import Component
 from pymunk import Body as Body
+import pygame as pg
+import os
 
 
-class Thruster(Component):
+class Thruster(ABC, Component):
     """Thruster component for rocket. Provides encapsulation for
        thrust direction and magnitude. Must be attached to a body.
 
@@ -18,16 +21,59 @@ class Thruster(Component):
     Attributes:
         _thrustVector (pymunk.Vec2d): direction of thrust (relative to Rocket).
         _thrustForce (float): magnitude of thrust.
-        fuel (int): current thruster fuel.
+        _fuel (int): current thruster fuel.
 
     """
 
-    def __init__(self, body, vertices, thrustVector,
-                 netThrust, transform=None, radius=0):
-        Component.__init__(self, body, vertices, transform, radius)
-        self._thrustVector = Vec2d(thrustVector)
-        self._thrustForce = netThrust
-        self.fuel = 40000
+    @abstractmethod
+    def __init__(self, body, transform=None, radius=0):
+        pass
+
+    @property
+    @abstractmethod
+    def vertices(self):
+        pass
+
+    @property
+    @abstractmethod
+    def thrustForce(self):
+        """Magnitude of thrust
+
+        Returns:
+            float: thrust magnitude.
+
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def thrustVector(self):
+        """Direction of thrust
+
+        Returns:
+            pymunk.Vec2d: direction of thrust adjusted for rocket rotation.
+
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def sprite(self):
+        pass
+
+    @property
+    @abstractmethod
+    def maxFuel(self):
+        pass
+
+    @property
+    def fuel(self):
+        return self._fuel
+
+    @fuel.setter
+    def fuel(self, newFuel):
+        if newFuel >= 0:
+            self._fuel = newFuel
 
     def thrust(self):
         """Gets the scaled thrust vector for application of forces
@@ -38,39 +84,6 @@ class Thruster(Component):
         """
         return self.thrustForce * self.thrustVector
 
-    @property
-    def thrustForce(self):
-        """Magnitude of thrust
-
-        Returns:
-            float: thrust magnitude.
-
-        """
-        return self._thrustForce
-
-    @thrustForce.setter
-    def thrustForce(self, netForce):
-        self._thrustForce = netForce
-
-    @property
-    def thrustVector(self):
-        """Direction of thrust
-
-        Returns:
-            pymunk.Vec2d: direction of thrust adjusted for rocket rotation.
-
-        """
-        if self._thrustVector is None:
-            self._thrustVector = Vec2d(0, 1)
-        tVect = Vec2d(self._thrustVector)
-        tVect.cpvrotate(self.body.rotation_vector)
-        return tVect
-
-    @thrustVector.setter
-    def thrustVector(self, v):
-        self._thrustVector = Vec2d(v).normalized()
-
-
     def applyThrust(self):
         if self.fuel > 0:
             self.body.apply_impulse_at_local_point(self.thrust(), (0, 0))
@@ -78,7 +91,7 @@ class Thruster(Component):
 
 
 """class RCS(Thruster):
-        def __init__(self, body, vertices, netThrust, transform, radius, fuel):
+        def __init__(self, body, vertices, netThrust, transform, radius, _fuel):
             Thruster.__init__(self, body, vertices, (1,0), newThrust, None, 0)
         #tell the thrust vector to go horiz
         def applyThrust(self, direction):
@@ -86,6 +99,40 @@ class Thruster(Component):
             if body.SASfuel > 0:
                 self.body.apply_impulse_at_local_pont(Thruster.thrust(), (0,0))"""
                 
+class DeltaVee(Thruster):
+
+    _vertices = [(4.2, 0), (-4.2, 0), (4.2, 46.9), (-4.2, 46.9)]
+    _thrustForce = 50000
+    _thrustVector = Vec2d((0,1))
+    _sprite = pg.image.load(os.path.join("assets", "sprites", "UpGoer2000.png"))
+    _maxFuel = 40000
+    
+    def __init__(self, body, transform=None, radius=0):
+        Component.__init__(self, body, self.vertices, transform, radius)
+        self._fuel = self.maxFuel
+
+    @property
+    def vertices(self):
+        return self._vertices
+
+    @property
+    def thrustForce(self):
+        return self._thrustForce
+
+    @property
+    def thrustVector(self):
+        return self._thrustVector
+
+    @property
+    def sprite(self):
+        return self._sprite
+
+    @property
+    def maxFuel(self):
+        return self._maxFuel
+
+
+
 
 #class SolidThruster(Thruster):
 
