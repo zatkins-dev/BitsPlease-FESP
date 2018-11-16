6cf257c90cb0c64cf307203fb1fd62a4c7075c61
Gage Phillips <Gagerdude@gmail.com>
2018-11-15 18:09:48 -0600

    Added command module to import list

--- rockets/__init__.py
+++ rockets/__init__.py
@@ -2,4 +2,5 @@ from rockets.component import Component
 from rockets.sas import SAS
 from rockets.thruster import Thruster, DeltaVee
 from rockets.rocket import Rocket
+from rockets.commandmodule import CommandModule
 import rockets.testrocket
