9bcc30ea8c3f6709ef9fd3e5f8989b98bf6c7b42
Gage Phillips <Gagerdude@gmail.com>
2018-11-17 17:15:40 -0600

    added the SAS subclasses to import list

--- rockets/__init__.py
+++ rockets/__init__.py
@@ -1,5 +1,5 @@
 from rockets.component import Component
-from rockets.sas import SAS
+from rockets.sas import SAS, AdvancedSAS
 from rockets.thruster import Thruster, DeltaVee, RCSThruster, RightRCS, LeftRCS
 from rockets.rocket import Rocket
 from rockets.commandmodule import CommandModule
