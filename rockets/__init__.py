1b9e8e91df5f29a1b51dcca024ba82018c26945c
Gage Phillips <Gagerdude@gmail.com>
2018-11-18 11:41:17 -0600

    Changed thruster import to wildcard *

--- rockets/__init__.py
+++ rockets/__init__.py
@@ -1,6 +1,6 @@
 from rockets.component import Component
 from rockets.sas import SAS, AdvancedSAS
-from rockets.thruster import Thruster, DeltaVee, RCSThruster, RightRCS, LeftRCS
+from rockets.thruster import *
 from rockets.rocket import Rocket
 from rockets.commandmodule import CommandModule
 import rockets.testrocket
