de0cb09a1e74c453fb24b2ad0541b17a2ffba4d4
Gage Phillips <gagerdude@gmail.com>
2018-11-23 11:21:17 -0600

    moved commandModule import to group with other components

--- rockets/__init__.py
+++ rockets/__init__.py
@@ -1,6 +1,6 @@
 from rockets.component import Component
 from rockets.sas import SAS, AdvancedSAS
 from rockets.thruster import *
-from rockets.rocket import Rocket
 from rockets.commandmodule import CommandModule
+from rockets.rocket import Rocket
 import rockets.testrocket
