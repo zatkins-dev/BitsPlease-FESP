6fe09df31f4f079ec6168721ef4d7f95eb64fc5e
marshingjay <jam605@gmail.com>
2018-11-25 17:54:45 -0600

    fixed audio assertion

--- tests.py
+++ tests.py
@@ -392,7 +392,7 @@ class AudioTestCase(unittest.TestCase):
 
         self.rocket.isAngleLocked = True
         self.audioManager.sasSoundEffect(len(self.rocket.SASmodules) != 0 and self.rocket.isAngleLocked)
-        self.assertFalse(pg.mixer.Channel(1).get_busy())
+        self.assertTrue(pg.mixer.Channel(1).get_busy())
 
         #Should stop sound effect
         self.audioManager.silenceMusic()
