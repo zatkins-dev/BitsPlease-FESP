d9c078fb8c4452ed45d114c3721bc8b6a1c2313c
marshingjay <jam605@gmail.com>
2018-11-24 22:59:54 -0600

    sand squid now has a reasonable amount of thrust

--- rockets/thruster.py
+++ rockets/thruster.py
@@ -391,7 +391,7 @@ class SandSquid(Thruster):
     def getInfo(cls):
         return {
             "vertices":     [(5, 0), (-5, 0), (-5, 5), (5, 5)],
-            "thrustForce":  550000,
+            "thrustForce":  55000,
             "thrustVector": Vec2d((0,1)),
             "sprite":       cls._sprite,
             "maxFuel":      1,
