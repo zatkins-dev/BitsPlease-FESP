27aa1fb4d2f772fd3f1896e3f2e4c52ef095af3d
marshingjay <jam605@gmail.com>
2018-11-25 11:22:36 -0600

    added test_append_component

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from rockets.testrocket import genRocket
-
+from rockets import Rocket, TestTank
 import pymunk as pm
 
 class RocketTestCase(unittest.TestCase):
@@ -11,3 +11,8 @@ class RocketTestCase(unittest.TestCase):
 
         def test_default_rocket(self):
             self.assertEqual(self.rocket.space, self.space)
+
+        def test_append_component(self):
+            self.newTank = TestTank(self.rocket)
+            self.rocket.addComponent(self.newTank)
+            self.assertIn(self.newTank, self.rocket.components)
