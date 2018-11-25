b25021ea55d02dcf4d05775a9dad76fdb65de6b4
Gage Phillips <Gagerdude@gmail.com>
2018-11-25 11:31:26 -0600

    finished testing an initial rocket

--- tests.py
+++ tests.py
@@ -1,13 +1,30 @@
 import unittest
 
 from rockets.testrocket import genRocket
+from rockets import *
 
 import pymunk as pm
 
 class RocketTestCase(unittest.TestCase):
         def setUp(self):
             self.space = pm.Space(threaded=True)
-            self.rocket = genRocket(self.space)
+            self.baseComponents = [CommandModule(None), UpGoer2000(None), AdvancedSAS(None), RightRCS(None), LeftRCS(None)]
+            self.rocket = Rocket(self.baseComponents)
 
         def test_default_rocket(self):
+            # test if rocket was actually added to our space
             self.assertEqual(self.rocket.space, self.space)
+
+            # test if the components were actually added to the rocket
+            self.assertIs(self.rocket.components, self.baseComponents)
+
+            # test initial conditions... destroyed, saslock, etc.
+            self.assertFalse(self.rocket.destroyed)
+            self.assertFalse(self.rocket.isAngleLocked)
+            self.assertEqual(self.rocket.throttle, 0)
+
+            # test that components bodies were made to be the rocket
+            for component in self.rocket.components:
+                self.assertIs(component.body, self.rocket)
+
+            
