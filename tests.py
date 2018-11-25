a5cc565fec5dc7e6ef2a83bee6a2135c3374ac66
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-11-25 12:02:24 -0600

    Test for thruster list

--- tests.py
+++ tests.py
@@ -1,8 +1,11 @@
 import unittest
+from graphics import Video
+Video.init()
 
 from rockets.testrocket import genRocket
-from rockets import Rocket, CommandModule, UpGoer2000, AdvancedSAS, RightRCS, LeftRCS, TestTank
+from rockets import Rocket, CommandModule, UpGoer2000, DeltaVee, SandSquid, AdvancedSAS, RightRCS, LeftRCS, TestTank
 import pymunk as pm
+
 
 class RocketTestCase(unittest.TestCase):
         def setUp(self):
@@ -31,3 +34,27 @@ class RocketTestCase(unittest.TestCase):
             self.rocket.addComponent(self.newTank)
             self.assertIn(self.newTank, self.rocket.components)
             self.tearDown()
+
+        def test_thruster_list(self):
+            self.thruster1 = UpGoer2000(self.rocket)
+            self.rocket.addComponent(self.thruster1)
+            self.assertIn(self.thruster1, self.rocket.thrusters)
+
+            self.thruster2 = DeltaVee(self.rocket)
+            self.rocket.addComponent(self.thruster2)
+            self.assertIn(self.thruster2, self.rocket.thrusters)
+
+            self.thruster3 = SandSquid(self.rocket)
+            self.rocket.addComponent(self.thruster3)
+            self.assertIn(self.thruster3, self.rocket.thrusters)
+
+            self.thruster4 = RightRCS(self.rocket)
+            self.rocket.addComponent(self.thruster4)
+            self.assertNotIn(self.thruster4, self.rocket.thrusters)
+
+            self.thruster5 = LeftRCS(self.rocket)
+            self.rocket.addComponent(self.thruster5)
+            self.assertNotIn(self.thruster5, self.rocket.thrusters)
+
+if __name__ == '__main__':
+    unittest.main()
