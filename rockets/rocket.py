580d14a7085ad89cdffa41b861a4688a6b03db8a
Gage Phillips <Gagerdude@gmail.com>
2018-11-24 19:06:11 -0600

    added docstrings to the rocket

--- rockets/rocket.py
+++ rockets/rocket.py
@@ -5,20 +5,17 @@ from audio import AudioManager
 
 
 class Rocket(Body):
-    """Extends pymunk.Body with helper functions for thruster & steering.
-
-    Args:
-        components (List(Component)): List of Components to attach to rocket.
-
-    Attributes:
-        thrusters (List(Thruster)): List of Thruster components.
-        SASmodules (List(SAS)): List of SAS components.
-        angular_velocity_limit (Float): Maximum angular velocity.
-        components (List(Component)): List of Components attached to rocket.
-
+    """
+    Extends pymunk.Body, and can hold and represent physics for the components of the rocket.
     """
 
     def __init__(self, components=[]):
+        """
+        Creates a Rocket with the given list of components.
+
+        :param components:
+        :type components: list(:py:class:`Component`)
+        """
         Body.__init__(self)
         for c in components:
             c.body = self
@@ -30,6 +27,9 @@ class Rocket(Body):
 
     @property
     def throttle(self):
+        """
+        The Throttle represents how much of the thrusters' power is being used, from 0 - 1.
+        """
         return self._throttle
 
     @throttle.setter
@@ -44,26 +44,45 @@ class Rocket(Body):
 
     @property
     def components(self):
+        """
+        A list of all of the individual :py:class:`Component`s that have been attatched to the rocket.
+        The list is presented in the order of :py:class:`Thruster`s, :py:class:`SAS`, :py:class:`CommandModule`s, :py:class:`RCSThruster`s.
+        """
         return self.thrusters + self.SASmodules + self.commandModules + self.RCSThrusters
 
     @property
     def commandModules(self):
+        """
+        A list of all of the :py:class:`CommandModule`s that have been attatched to the rocket.
+        """
         return list(filter(lambda c: isinstance(c, CommandModule), self._components))
 
     @property
     def thrusters(self):
+        """
+        A list of all of the :py:class:`Thruster`s - but not :py:class:`RCSThruster`s - that have been attatched to the rocket.
+        """
         return list(filter(lambda c: isinstance(c, Thruster) and not isinstance(c, RCSThruster), self._components))
 
     @property
     def RCSThrusters(self):
+        """
+        A list of all of the :py:class:`RCSThruster`s that have been attatched to the rocket.
+        """
         return list(filter(lambda c: isinstance(c, RCSThruster), self._components))
 
     @property
     def SASmodules(self):
+        """
+        A list of all of the :py:class:`SAS`Modules that have been attatched to the rocket.
+        """
         return list(filter(lambda c: isinstance(c, SAS), self._components))
 
     @property
     def isAngleLocked(self):
+        """
+        Represents whether or not the rocket is attempting to hold itself to a certain angle.
+        """
         return self._isAngleLocked
 
     @isAngleLocked.setter
@@ -77,6 +96,9 @@ class Rocket(Body):
 
 
     def tick(self, timescale):
+        """
+        Allows the rocket to perform its functions, checking input and applying thrust and rotation to itself.
+        """
         # grab the keyboard state
         currentKeys = pg.key.get_pressed()
 
@@ -111,6 +133,9 @@ class Rocket(Body):
 
 
     def handleEvent(self, event):
+        """
+        A pygame event handler, currently only checks for the SAS angle-toggle key V
+        """
         if event.type is pg.KEYDOWN:
             # new key has been pressed, place edge sensitive
             # commands in here, i.e. things that only run once
@@ -120,26 +145,42 @@ class Rocket(Body):
 
 
     def addComponent(self, c):
-        """Add a new component to the rocket
-
-        Args:
-            c (Component): Component to attach to rocket
-
         """
+        Add a new component to the rocket
+
+        :param c: The component to add to the rocket
+        :type c: :py:class:`Component`
+        """
+
         c.body = self
         self._components.append(c)
 
     def removeComponent(self, c):
+        """
+        Removes a component from the rocket
+
+        :param c: The component to remove from the rocket
+        :type c: :py:class:`Component`
+        """
+
         for x in self.components:
             if x.get_vertices() == c.get_vertices() :
                 self._components.remove(x)
 
 
     def debugComponentPrint(self):
+        """
+        Prints out the verticies of all the components in this rocket.
+        """
         for x in self.components :
             print(x.get_vertices())
 
     def reset(self):
+        """
+        Resets information about this rocket for purposes of restarting the simulation.
+        This includes the destroyed flag, the position, velocity, angle, throttle, isAngleLocked,
+        and also resets every component of the rocket.
+        """
         self.destroyed = False
         self.position = (0,0)
         self.velocity = (0,0)
