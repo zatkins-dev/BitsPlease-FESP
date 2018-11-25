722afcc0f2f25ab48e5d041ac190f35d96d3d65d
Gage Phillips <Gagerdude@gmail.com>
2018-11-24 18:11:27 -0600

    Rewrote docstrings

--- rockets/component.py
+++ rockets/component.py
@@ -4,21 +4,24 @@ from physics.collision import CT_COMPONENT
 from abc import ABC, abstractmethod
 
 class Component(ABC, pm.Poly):
-    """Extention of pymunk Poly class with properties for sprites/textures.
-
-    Args:
-        body (Body): Body to attach component to
-        vertices (List(Vec2d)): Vertices of Poly shape
-        transform (Transform): Transformation to apply to shape
-        radius (Float): Edge radius of shape for smoothing
-
-    Attributes:
-        _vertices (list of float, float): list of tuples holding x,y coordinates
-        _sprite (pygame.Surface): pygame Surface holding image of component
-
+    """
+    Extention of pymunk Poly class with properties for sprites/textures and shape (vertices)
     """
 
     def __init__(self, body, vertices, transform=None, radius=0):
+        """
+        Passes the given parameters on to the underlying Pymunk shape, and initializes
+        some collision management and the destroyed status of the component.
+
+        :param body: Body to attatch the component to.
+        :type body: :py:class:`pymunk.Body`
+        :param vertices: Verticies of the shape
+        :type vertices: list(:py:class:`pymunk.vec2d.Vec2d`)
+        :param transform: Transformation to apply to the shape
+        :type transform: :py:class:`pymunk.Transform`
+        :param float radius: Radius of the shape, used for smoothing.
+        """
+
         super().__init__(body, vertices, transform, radius)
         self.collision_type = CT_COMPONENT
         self.destroyed = False
@@ -26,21 +29,31 @@ class Component(ABC, pm.Poly):
     @property
     @abstractmethod
     def vertices(self):
+        """
+        This should return the vertices of the component
+        """
         pass
 
     @property
     @abstractmethod
     def sprite(self):
+        """
+        This should return a surface that represents the component's sprite/texture
+        """
         pass
 
     @classmethod
     @abstractmethod
     def getDisplayInfo(cls):
-        """This will should return a dictionary that contains relevant
-           Information to display in the rocket builder
+        """
+        This will should return a dictionary that contains relevant
+        "pretty" Information to display in the rocket builder
         """
         pass
 
     @abstractmethod
     def reset(self):
+        """
+        Resets the destroyed status of the component.
+        """
         self.destroyed = False
